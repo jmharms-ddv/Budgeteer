874f558fb7242f169576065ca41c8e8374a9bc19
Aaron <aaron@ironmthome.com>
2019-11-02 01:41:16 -0400

    Added getBills, getBill, and postBill to income api (js)

--- resources/js/api/bill.js
+++ resources/js/api/bill.js
@@ -1,8 +1,45 @@
 /*
     Imports the Budgeteer API URL from the config.
 */
-import { BUDGETEER_CONFIG } from '../config.js’;
+import { BUDGETEER_CONFIG } from '../config.js';
 
 export default {
+  /*
+      GET     /api/bill
+      @param options {object}
+        can contain with [array]
+      @return Promise
+  */
+  getBills: function(options) {
+    let optionsStr = '';
+    if(options) {
+      optionsStr = '?';
+      if(options.hasOwnProperty('with') && options.with.length != 0) {
+        optionsStr += (optionsStr == '?' ? 'with=' + options.with[0] : '&with=' + options.with[0]);
+        for(let i in options.with) {
+          if(i == 0) continue;
+          optionsStr += ':' + options.with[i];
+        }
+      } else {
+        optionsStr = '';
+      }
+    }
+    return axios.get(BUDGETEER_CONFIG.API_URL + '/bill' + optionsStr);
+  },
 
+  /*
+      GET     /api/bill/{id}
+  */
+  getBill: function(id) {
+    return axios.get(BUDGETEER_CONFIG.API_URL + '/bill/' + id);
+  },
+
+  /*
+      POST     /api/income
+  */
+  postBill: function(name) {
+    return axios.post(BUDGETEER_CONFIG.API_URL + '/bill', {
+      name: name
+    });
+  }
 }
