afcb035bcb3981e5227a0cab94a9ced482ebc40b
Aaron <aaron@ironmthome.com>
2019-11-02 01:39:22 -0400

    Added bills api routes

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,10 @@ Route::post('income', ['as' => 'income.store', 'uses' => 'IncomesController@stor
 Route::put('income', ['as' => 'income.store', 'uses' => 'IncomesController@store']);
 Route::delete('income/{income}', ['as' => 'income.destroy', 'uses' => 'IncomesController@destroy']);
 
+Route::get('bill', ['as' => 'bill.index', 'uses' => 'BillsController@index']);
+Route::get('bill/{bill}', ['as' => 'bill.show', 'uses' => 'BillsController@show']);
+Route::post('bill', ['as' => 'bill.store', 'uses' => 'BillsController@store']);
+Route::put('bill', ['as' => 'bill.store', 'uses' => 'BillsController@store']);
+Route::delete('bill/{bill}', ['as' => 'bill.destroy', 'uses' => 'BillsController@destroy']);
+
 Route::get('user', ['as' => 'user.loggedin', 'uses' => 'UsersController@loggedin']);
