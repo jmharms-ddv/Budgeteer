380b70399577f0321371de912e1109ce4b584526
Aaron <aaron@ironmthome.com>
2019-11-02 01:42:03 -0400

    Added bills VUEX module (js) inital commit

---
+++ resources/js/modules/bills.js
@@ -0,0 +1,106 @@
+/*
+|-------------------------------------------------------------------------------
+| VUEX modules/bills.js
+|-------------------------------------------------------------------------------
+| The Vuex data store for the bills
+*/
+
+import BillAPI from '../api/bill.js';
+
+export const bills = {
+  state: {
+    bills: [],
+    billsLoadStatus: 0,
+
+    bill: {},
+    billLoadStatus: 0,
+
+    addBillStatus: 0
+  },
+
+  actions: {
+    loadBills({ commit }, data) {
+      commit('setBillsLoadStatus', 1);
+      let options = data.with ? data.with : [];
+      BillAPI.getBills({ with: options })
+        .then(res => {
+          commit('setBills', res.data.data);
+          commit('setBillsLoadStatus', 2);
+        })
+        .catch(err => {
+          commit('setBills', []);
+          commit('setBillsLoadStatus', 3);
+        });
+    },
+    loadBill({ commit }, data) {
+      commit('setBillLoadStatus', 1);
+
+      BillAPI.getBill(data.id)
+        .then(res => {
+          commit('setBill', res.data.data);
+          commit('setBillLoadStatus', 2);
+        })
+        .catch(err => {
+          commit('setBill', {});
+          commit('setBillLoadStatus', 3);
+        });
+
+    },
+    addBill({ commit, state, dispatch }, data) {
+      commit('setAddBillStatus', 1);
+
+      BillAPI.postBill(data.name)
+        .then(res => {
+          commit('setAddBillStatus', 2);
+          dispatch('loadBills');
+        })
+        .catch(err => {
+          commit('setAddBillStatus', 3);
+        });
+    }
+  },
+
+  mutations: {
+    setBillsLoadStatus(state, status) {
+      state.billsLoadStatus = status;
+    },
+
+    setBills(state, bills) {
+      state.bills = bills;
+    },
+
+    setBillLoadStatus(state, status) {
+      state.billLoadStatus = status;
+    },
+
+    setBill(state, bill) {
+      state.bill = bill;
+    },
+
+    setAddBillStatus(state, status) {
+      state.addBillStatus = status;
+    }
+  },
+
+  getters: {
+    getBillsLoadStatus(state) {
+      return state.billsLoadStatus;
+    },
+
+    getBills(state) {
+      return state.bills;
+    },
+
+    getBillLoadStatus(state) {
+      return state.billLoadStatus;
+    },
+
+    getBill(state){
+      return state.bill;
+    },
+
+    getAddBillStatus(state) {
+      return state.addBillStatus;
+    }
+  }
+}
