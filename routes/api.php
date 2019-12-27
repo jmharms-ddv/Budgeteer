42d382faebb86fb5821a2f203d239ae758c29002
Aaron <aaron@ironmthome.com>
2019-12-27 15:03:03 -0500

    Added separate API routes for post and put and all Paycheck routes

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,19 @@ use Illuminate\Support\Facades\Auth;
 Route::get('income', ['as' => 'income.index', 'uses' => 'IncomesController@index']);
 Route::get('income/{income}', ['as' => 'income.show', 'uses' => 'IncomesController@show']);
 Route::post('income', ['as' => 'income.store', 'uses' => 'IncomesController@store']);
-Route::put('income', ['as' => 'income.store', 'uses' => 'IncomesController@store']);
+Route::put('income', ['as' => 'income.update', 'uses' => 'IncomesController@update']);
 Route::delete('income/{income}', ['as' => 'income.destroy', 'uses' => 'IncomesController@destroy']);
 
 Route::get('bill', ['as' => 'bill.index', 'uses' => 'BillsController@index']);
 Route::get('bill/{bill}', ['as' => 'bill.show', 'uses' => 'BillsController@show']);
 Route::post('bill', ['as' => 'bill.store', 'uses' => 'BillsController@store']);
-Route::put('bill', ['as' => 'bill.store', 'uses' => 'BillsController@store']);
+Route::put('bill', ['as' => 'bill.update', 'uses' => 'BillsController@update']);
 Route::delete('bill/{bill}', ['as' => 'bill.destroy', 'uses' => 'BillsController@destroy']);
+
+Route::get('paycheck', ['as' => 'paycheck.index', 'uses' => 'PaychecksController@index']);
+Route::get('paycheck/{paycheck}', ['as' => 'paycheck.show', 'uses' => 'PaychecksController@show']);
+Route::post('paycheck', ['as' => 'paycheck.store', 'uses' => 'PaychecksController@store']);
+Route::put('paycheck', ['as' => 'paycheck.update', 'uses' => 'PaychecksController@update']);
+Route::delete('paycheck/{paycheck}', ['as' => 'paycheck.destroy', 'uses' => 'PaychecksController@destroy']);
 
 Route::get('user', ['as' => 'user.loggedin', 'uses' => 'UsersController@loggedin']);
