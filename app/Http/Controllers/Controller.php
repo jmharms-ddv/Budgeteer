1f00fb85e3732d7731c143e65629e9739618e664
Aaron <aaron@ironmthome.com>
2019-12-27 14:49:56 -0500

    Added extractOptions function to generic controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function extractOptions(Request $request) {
+        $withArr = ($request->has('with') && !empty($request->input('with'))) ? explode(':', $request->input('with')) : [];
+        $filter_dateArrOfStrs = ($request->has('filter_date') && !empty($request->input('filter_date'))) ? explode(':', $request->input('filter_date')) : [];
+        $filter_dateArr = [];
+        foreach($filter_dateArrOfStrs as $dateCompareStr) {
+            $compareArr = explode('|', $dateCompareStr);
+            array_push($filter_dateArr, [
+              'comparison' => $compareArr[0],
+              'comparison_value' => $compareArr[1],
+              'model_attribute' => $compareArr[2]
+            ]);
+        }
+        return [
+          'with' => $withArr,
+          'filter_date' => $filter_dateArr
+        ];
+    }
 }
