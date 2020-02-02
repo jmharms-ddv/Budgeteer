89eb7b38f372f4113db91c2d9771de2ce5b3d2f9
Aaron <aaron@ironmthome.com>
2020-02-01 19:45:04 -0500

    Moved ItemBill, ItemPaycheck components to bills, paychecks folders

--- resources/js/components/bills/ItemBill.vue
+++ resources/js/components/bills/ItemBill.vue
@@ -3,7 +3,7 @@
     <template v-if="(paycheck.pivot_amount || paycheck.pivot_amount_project) && (paycheck.pivot_paid_on == null)">
       <div class="d-flex justify-content-between">
         <h5 class="card-title">
-          {{ value.name }}
+          {{ bill.name }}
         </h5>
         <h5 class="card-title">
           ${{ (paycheck.pivot_amount == null ? paycheck.pivot_amount_project : paycheck.pivot_amount) }}
@@ -13,13 +13,13 @@
         </h5>
       </div>
       <div class="d-flex justify-content-between">
-        <small class="text-muted">Scheduled on {{ paycheck.paid_on }}</small>
+        <small class="text-muted">Scheduled on {{ paycheck_paid_on }}</small>
       </div>
     </template>
     <template v-else-if="(paycheck.pivot_amount || paycheck.pivot_amount_project) && (paycheck.pivot_paid_on != null)">
       <div class="d-flex justify-content-between">
         <h5 class="card-title">
-          {{ value.name }}
+          {{ bill.name }}
         </h5>
         <h5 class="card-title">
           ${{ (paycheck.pivot_amount == null ? paycheck.pivot_amount_project : paycheck.pivot_amount) }}
@@ -29,27 +29,27 @@
         </h5>
       </div>
       <div class="d-flex justify-content-between">
-        <small class="text-muted">Paid on {{ paycheck.pivot_paid_on }}</small>
+        <small class="text-muted">Paid on {{ paycheck_pivot_paid_on }}</small>
       </div>
     </template>
     <template v-else>
       <div class="d-flex justify-content-between">
         <h5 class="card-title">
-          {{ value.name }}
+          {{ bill.name }}
         </h5>
         <h5 class="card-title">
-          ${{ value.amount }}
+          ${{ bill.amount }}
         </h5>
         <h5 class="card-title text-base">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.917 52.917" height="26" width="26"><path d="M52.13 26.458A25.672 25.672 0 0126.458 52.13 25.672 25.672 0 01.786 26.458 25.672 25.672 0 0126.458.786 25.672 25.672 0 0152.13 26.458z" fill="none" stroke="#1D4880" stroke-width="1.573"/></svg>
         </h5>
       </div>
       <div class="d-flex justify-content-between">
-        <small class="text-muted">Due on {{ value.day_due_on }}</small>
+        <small class="text-muted">Due on {{ bill_day_due_on }}</small>
       </div>
     </template>
     <div v-if="highlight && !receivingPair" class="d-flex justify-content-between mt-2">
-      <button class="btn btn-outline-sub1 btn-sm" @click="$emit('edit')">Edit</button>
+      <button class="btn btn-outline-sub1 btn-sm" @click="onModify()">Edit</button>
       <button v-if="paycheck.hasOwnProperty('id')"
               type="button"
               class="btn btn-outline-base btn-sm"
@@ -58,7 +58,7 @@
               type="button"
               class="btn btn-outline-base btn-sm"
               @click="onPair()">Pair</button>
-      <button class="btn btn-outline-sub2 btn-sm" @click="$emit('delete')">Delete</button>
+      <button class="btn btn-outline-sub2 btn-sm" @click="onDelete()">Delete</button>
     </div>
     <div v-if="receivingPair" class="text-center mt-2">
       <button type="button"
@@ -79,11 +79,11 @@
 </style>
 
 <script>
-  import { EventBus } from '../event-bus.js';
-
+  import { EventBus } from '../../event-bus.js';
+  import moment from 'moment';
   export default {
     props: {
-      value: {
+      bill: {
         type: Object,
         required: true
       },
@@ -108,16 +108,10 @@
         default: false
       }
     },
-
     created() {
       EventBus.$on('paycheck-pair-start', obj => {
+        this.receivingPair = true;
         this.$emit('bill-stay-highlighted', true);
-      });
-      EventBus.$on('bill-pair-end', arr => {
-        //this.$emit('bill-highlight', false);
-      });
-      EventBus.$on('bill-pair-start', arr => {
-        //if(arr[0].id != this.value.id) this.pairMode[0] = false;
       });
       EventBus.$on('paycheck-pair-end', obj => {
         if(this.receivingPair) {
@@ -126,41 +120,51 @@
         }
       });
     },
-
     data() {
       return {
         receivingPair: false
       };
     },
-
     methods: {
-      onPairUpdate() {
-        EventBus.$emit('pair-update', [this.value, this.paycheck]);
+      onModify() {
+        EventBus.$emit('modify-bill', this.bill);
       },
+      onDelete() {
 
+      },
+      onPairUpdate() {
+        EventBus.$emit('pair-update', [this.bill, this.paycheck, 'paycheck']);
+      },
       onPair() {
         if(!this.receivingPair) {
           // case: the bill is selected first
           this.receivingPair = true;
           this.$emit('bill-stay-highlighted', true);
-          EventBus.$emit('bill-pair-start', [this.value, this.month]);
+          EventBus.$emit('bill-pair-start', [this.bill, this.month]);
         } else {
-          EventBus.$emit('bill-pair-end', [this.value, this.month]);
+          EventBus.$emit('bill-pair-end', [this.bill, this.month]);
         }
       }
     },
-
     computed: {
       thisMonth() {
         let monthStr = "" + this.month[1] + "-" + (this.month[0] > 9 ? this.month[0] : "0" + this.month[0]);
         return monthStr;
       },
-
       paycheck() {
-        for(let i in this.value.paychecks) {
-          if(this.month[1] + "-" + (this.month[0] > 9 ? this.month[0] : "0" + this.month[0]) == this.value.paychecks[i].pivot_due_on.substr(0, 7)) return this.value.paychecks[i];
+        for(let i in this.bill.paychecks) {
+          if(this.month[1] + "-" + (this.month[0] > 9 ? this.month[0] : "0" + this.month[0]) == this.bill.paychecks[i].pivot_due_on.substr(0, 7)) return this.bill.paychecks[i];
         }
         return {};
+      },
+      paycheck_paid_on() {
+        return moment(this.paycheck.paid_on).format('ddd, MMM D');
+      },
+      paycheck_pivot_paid_on() {
+        return moment(this.paycheck.pivot_paid_on).format('ddd, MMM D');
+      },
+      bill_day_due_on() {
+        return moment([this.month[1], this.month[0] - 1, this.bill.day_due_on]).format('ddd, MMM D');
       }
     }
   }
