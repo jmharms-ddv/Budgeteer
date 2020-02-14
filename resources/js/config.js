c12b1cef1bd2137334ff8a8dbb7a5dc5656d2018
Aaron <aaron@ironmthome.com>
2020-02-14 12:33:05 -0500

    Added URL switch based on Node env (dev/production)

--- resources/js/config.js
+++ resources/js/config.js
@@ -6,9 +6,11 @@ var api_url = url + '/api';
 
 switch( process.env.NODE_ENV ){
   case 'development':
+    url = 'http://budgeteer.homestead';
     api_url = url + '/api';
   break;
   case 'production':
+    url = 'https://www.budgeteer.ironmthome.com';
     api_url = url + '/api';
   break;
 }
